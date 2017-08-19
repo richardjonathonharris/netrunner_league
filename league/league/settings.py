df4e3268b074d758f5a35f5b4568b14a22ebf1b1
Richard Harris <richard.jonathon.harris@gmail.com>
2017-08-19 21:34:33 +0000

    Django app and working db

--- league/league/settings.py
+++ league/league/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'league_tracker'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'league.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': os.environ['DBPASSWORD'],
+        'HOST': 'localhost',
     }
 }
 
---
+++ league/league_tracker/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LeagueTrackerConfig(AppConfig):
+    name = 'league_tracker'
