a4ef38200757e3d5078abcc61864bf3668aa2cb9
Richard Harris <richard.jonathon.harris@gmail.com>
2017-09-23 15:16:57 +0000

    Trying for elastic beanstalk deployment

--- league/league/settings.py
+++ league/league/settings.py
@@ -75,16 +75,28 @@ WSGI_APPLICATION = 'league.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'django_db',
-        'USER': 'root',
-        'PASSWORD': os.environ['DBPASSWORD'],
-        'HOST': '',
-        'PORT': '',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django_db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_POST'],
+            }
+        }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'django_db',
+            'USER': 'root',
+            'PASSWORD': os.environ['DBPASSWORD'],
+            'HOST': '',
+            'PORT': '',
+        }
     }
-}
 
 
 # Password validation
@@ -124,3 +136,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'www', 'static')
